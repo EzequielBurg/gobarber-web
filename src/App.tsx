6088caf4543a26f5027f04444e2ecac98aded691
Ezequiel Gonçalves <ezekiel_sci@hotmail.com>
2020-07-28 23:24:36 -0300

    Changes until toasts with styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
-    <AuthContext.Provider value={{ name: 'Ezequiel' }}>
+    <AuthProvider>
       <SignIn />
-    </AuthContext.Provider>
+    </AuthProvider>
+
+    <ToastContainer />
+
     <GlobalStyle />
   </>
 );
